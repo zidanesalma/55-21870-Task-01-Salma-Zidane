43b22a74c1ef091a3a3ba3ee9ca2a5bc92007e2b
Salma Zidane <zidanesalma080@gmail.com>
2026-03-15 00:08:33 +0200

    fixed typo in welcome

--- src/main/java/s55_21870/t_21/salma_zidane/controllers/WelcomeController.java
+++ src/main/java/s55_21870/t_21/salma_zidane/controllers/WelcomeController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/welcome")
 public class WelcomeController {
 
     @Value("${USER_NAME}")
