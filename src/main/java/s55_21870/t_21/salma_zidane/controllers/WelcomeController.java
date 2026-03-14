82494b137e531fb734f10e5f224b933e5d351f2d
Salma Zidane <zidanesalma080@gmail.com>
2026-03-14 23:46:04 +0200

    feat: add welcome endpoint

---
+++ src/main/java/s55_21870/t_21/salma_zidane/controllers/WelcomeController.java
@@ -0,0 +1,24 @@
+package s55_21870.t_21.salma_zidane.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/welcome")
+public class WelcomeController {
+
+    @Value("${USER_NAME}")
+    private String userName;
+
+    @Value("${ID}")
+    private String id;
+
+    // GET /welcome endpoint
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Hello " + userName + " " + id + ", from Notes API";
+    }
+
+}
