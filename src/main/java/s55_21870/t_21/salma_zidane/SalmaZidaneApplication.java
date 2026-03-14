eb02392f09d1efc68848f6d4b0d857fc419711c3
Salma Zidane <zidanesalma080@gmail.com>
2026-03-14 19:49:04 +0200

    Initial commit: project setup

---
+++ src/main/java/s55_21870/t_21/salma_zidane/SalmaZidaneApplication.java
@@ -0,0 +1,13 @@
+package s55_21870.t_21.salma_zidane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalmaZidaneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalmaZidaneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/s55_21870/t_21/salma_zidane/SalmaZidaneApplicationTests.java
@@ -0,0 +1,13 @@
+package s55_21870.t_21.salma_zidane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalmaZidaneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
